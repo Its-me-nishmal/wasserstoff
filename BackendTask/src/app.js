253ca8a358d087683708897499846bfee5b23203
alltrackerx <alltrackerx@gmail.com>
2024-07-04 06:48:25 +0000

    vercel configeration settup

--- BackendTask/src/app.js
+++ BackendTask/src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan'
 const app = express();
 
 app.use(morgan('dev'));
+app.use('/',(req,res) => res.json({messge:'server is running'}))
 app.use('/api',loadBalancer);
 
 export default app;
