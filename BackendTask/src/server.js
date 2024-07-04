35c432851a8699a767e15f473e68885eae848491
alltrackerx <alltrackerx@gmail.com>
2024-07-04 10:17:01 +0000

    pm2 removed

--- BackendTask/src/server.js
+++ BackendTask/src/server.js
@@ -1,10 +1,13 @@
-import app from './app.js'
+import app from './app.js';
 import './mockServers.js';
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-    console.log(`Server is running on port https://localhost:${PORT}`);
-  
+  console.log(`Server is running on port https://localhost:${PORT}`);
 });
+
+// Keep the server active
+setInterval(() => {
+  console.log('Server is still running...');
+}, 10000); // This will log every 10 seconds
