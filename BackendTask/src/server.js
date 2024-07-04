88f364237b7e8014bcd7909bab8fa09270e5bc47
alltrackerx <alltrackerx@gmail.com>
2024-07-04 08:29:57 +0000

    testing

--- BackendTask/src/server.js
+++ BackendTask/src/server.js
@@ -1,15 +1,17 @@
 import app from './app.js'
 import mockserver from './mockServers.js';
 
-mockserver('4564', 200, 'WeatherAPI1');
-mockserver('6576', 100, 'WeatherAPI2');
-mockserver('3020', 300, 'WeatherAPI3');
-mockserver('8045', 250, 'WeatherAPI4');
-mockserver('9948', 150, 'WeatherAPI5');
+
 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+
     console.log(`Server is running on port https://localhost:${PORT}`);
+    mockserver('4564', 200, 'WeatherAPI1');
+    mockserver('6576', 100, 'WeatherAPI2');
+    mockserver('3020', 300, 'WeatherAPI3');
+    mockserver('8045', 250, 'WeatherAPI4');
+    mockserver('9948', 150, 'WeatherAPI5');
 });
