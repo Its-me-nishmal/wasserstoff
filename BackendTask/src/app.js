5c029a5324fbf3b667932814d9b529052e3f5f62
alltrackerx <alltrackerx@gmail.com>
2024-07-04 07:56:48 +0000

    error fix for server starting

--- BackendTask/src/app.js
+++ BackendTask/src/app.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import loadBalancer from './routes/loadBalancerRoutes.js'
-import morgan from 'morgan'
+import express from 'express';
+import loadBalancer from './routes/loadBalancerRoutes.js';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use('/',(req,res) => res.json({messge:'server is running'}))
-app.use('/api',loadBalancer);
+app.get('/', (req, res) => res.json({ message: 'server is running' })); // Fixed typo and used .get for the root route
+app.use('/api', loadBalancer);
 
 export default app;
