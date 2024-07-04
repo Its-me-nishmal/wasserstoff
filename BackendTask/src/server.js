f77854a7079a394794d0349e5b433f48f54a8596
alltrackerx <alltrackerx@gmail.com>
2024-07-04 08:09:47 +0000

    test

--- BackendTask/src/server.js
+++ BackendTask/src/server.js
@@ -1,11 +1,11 @@
 import app from './app.js'
 import mockserver from './mockServers.js';
 
-mockserver('3001', 200, 'WeatherAPI1');
-mockserver('3002', 100, 'WeatherAPI2');
-mockserver('3003', 300, 'WeatherAPI3');
-mockserver('3004', 250, 'WeatherAPI4');
-mockserver('3005', 150, 'WeatherAPI5');
+// mockserver('3001', 200, 'WeatherAPI1');
+// mockserver('3002', 100, 'WeatherAPI2');
+// mockserver('3003', 300, 'WeatherAPI3');
+// mockserver('3004', 250, 'WeatherAPI4');
+// mockserver('3005', 150, 'WeatherAPI5');
 
 
 const PORT = process.env.PORT || 3000;
